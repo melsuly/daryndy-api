b562911ff7d6ed192dd51842eb5b4c85e0999eef
Nurasyl Melsuly <nurasylmelsuly@gmail.com>
2023-05-28 01:40:04 +0600

    Connect to MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = 4444
+const mongodbURI = 'mongodb+srv://admin:AHLWvG1PiH6AugiU@daryndyv1.p076h8a.mongodb.net/v1?retryWrites=true&w=majority'
+
+// Connect to DB
+mongoose.connect(mongodbURI) 
+    .then(() => console.log('MongoDB is connected successfully!'))
+    .catch((e) => console.warn('Error in connecting to database', e))
 
 // Enable JSON
 app.use(express.json())
@@ -10,9 +17,9 @@ app.use(express.json())
 app.use('/auth', require('./routes/auth.routes'))
 
 // Start server and display errors & logs
-app.listen(port, (err) => {
-    if (err) {
-        return console.warn(err)
+app.listen(port, (e) => {
+    if (e) {
+        return console.warn('Error in starting server:', e)
     }
 
     console.log('Server is started at port', port)
